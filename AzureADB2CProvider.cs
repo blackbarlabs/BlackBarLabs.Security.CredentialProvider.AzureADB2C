6e576284c18c6cf5fc4a27471404432a50a59fdf
Keith Holloway <keith@eastfive.com>
2016-09-08 07:55:33 -0500

    Genesis

---
+++ AzureADB2CProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.CredentialProvider.AzureADB2C
+{
+    public class AzureADB2CProvider : IProvideCredentials
+    {
+        public async Task<TResult> RedeemTokenAsync<TResult>(Uri providerId, string username, string token, 
+            Func<string, TResult> success, Func<string, TResult> invalidCredentials,
+            Func<TResult> couldNotConnect)
+        {
+
+            //TODO - Validate the token with AAD B2C here
+
+            return success(token);
+        }
+
+        public Task<TResult> UpdateTokenAsync<TResult>(Uri providerId, string username, string token, Func<string, TResult> success, Func<TResult> doesNotExist,
+            Func<TResult> updateFailed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResult> GetCredentialsAsync<TResult>(Uri providerId, string username, Func<string, TResult> success, Func<TResult> doesNotExist)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
