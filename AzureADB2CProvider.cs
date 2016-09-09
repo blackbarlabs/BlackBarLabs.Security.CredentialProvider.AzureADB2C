4366319cd297659c070c6ed0c07c515ecab1f208
Keith Holloway <keith@eastfive.com>
2016-09-09 08:43:55 -0500

    Added notes on token validation back to AADB2C

--- AzureADB2CProvider.cs
+++ AzureADB2CProvider.cs
@@ -11,8 +11,17 @@ namespace BlackBarLabs.Security.CredentialProvider.AzureADB2C
         {
 
             //TODO - Validate the token with AAD B2C here
+            //Surely there is a library for this.  Actually, the OWIN library does this.  Look in OO API's Startup.Auth.cs.
+            //If that cannot be leveraged, to get the public key:
+            //https://login.microsoftonline.com/humagelorderowladb2cdev.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=b2c_1_sign_up_sign_in
+            //from there, follow the jwks_uri to here:
+            //https://login.microsoftonline.com/humagelorderowladb2cdev.onmicrosoft.com/discovery/v2.0/keys?p=b2c_1_sign_up_sign_in
+            //This will return the rolling keys to validate the jwt signature
+            //We will want to cache the key here and only go fetch again if the signature look up fails.  The keys rotate about every 24 hours.
 
-            return success(token);
+            return success("key");
+
+
         }
 
         public Task<TResult> UpdateTokenAsync<TResult>(Uri providerId, string username, string token, Func<string, TResult> success, Func<TResult> doesNotExist,
